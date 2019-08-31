86242de0b6a3e8e9b51c600ce9899cc2363e8bf8
Chirag Dugar <c.dugar2011@gmail.com>
2019-08-31 23:58:56 +0530

    Final

---
+++ App.js
@@ -0,0 +1,119 @@
+import React from 'react';
+import { StyleSheet, Text, View,ScrollView  } from 'react-native';
+
+export default function App() {
+  return (
+    <ScrollView>
+      <View style={ styles.container } > 
+        <Text style={ styles.heading1 } >Privacy Policy</Text>
+        <Text style={ styles.heading2 } >Welcome to the Privacy Policy!{'\n'}</Text>
+        <Text style={ styles.maincontent } >
+          We are a information provider service connecting people in the market that have a property to rent. Sigma Tenant and our subsidiaries and affiliates (collectively "Sigma Tenant") provide you with access to a variety of services, including but not limited to the www.sigmatenant.com website (the "Website") and all the products and services available through our website (collectively the "Services"). The Website is owned and operated by SigmaTenant.
+          Our Privacy Policy is incorporated as part of the Sigma Tenant Terms of Use. Please read this Privacy Policy and our Terms of Use carefully before using our Services. By using Sigma Tenant’s Services, you agree to the provisions of this Privacy Policy. If you do not want to be bound by our Privacy Policy, please do not use our Services.
+          
+          <Text style = { styles.innercontent } >
+          {'\n'}{'\n'}Why We Collect Information from You?{'\n'}{'\n'}
+          </Text>
+          
+          Sigma Tenant is a information provider service. To provide our Services, we use the information we collect to :
+          Help you find the most relevant information for your situation by customizing our Services to optimize your experience;
+          Keep you connected with Sigma Tenant across the internet and update you with residential property related information;
+          Put you in touch with the right people to sell/rent, or research residential real estate, obtain a mortgage, and to optimize the information we share with those parties to initiate a productive and efficient relationship.
+          Our goal in this policy is to be completely transparent about the data we collect, why we collect it, and how we use it, in order to make finding your new home a pleasant and fun experience!
+          
+          <Text style = { styles.innercontent }>
+          {'\n'}{'\n'}To Whom Does This Policy Apply?{'\n'}{'\n'}
+          </Text>
+
+          Some parts of this Policy are directed at specific types of people. We use some special terminology to refer to them, which we define below. This Policy applies to everyone who uses our Services, but sometimes we direct parts of it toward particular groups of users.
+          Users: "Users" are everyone who uses our Services.
+          Real Estate Professionals (REPs): "Real Estate Professionals," or "REPs" are users who have created accounts (free or paid) with us to sell or rent real estate, participate in our advertising services, and participate in other Services that we direct toward the professional real estate community. REPs include landlords, agents, developers, institutional property consultants, mortgage professionals and subscribers. We send leads to REPs on your behalf, as described in this Policy.
+          What Information Do We Collect, and Where Do We Get It
+          We Collect Some Information From All Visitors to Our Website: Like many online services, we use technologies like session and persistent cookies, web beacons (tiny image files on web pages that communicate information about the page viewer to the beacon owner), log data, and third-party analytics services to collect and analyze information about all users of our Services This includes things like User search preferences, interaction with ads on our Website, and location.
+          Our servers automatically record information ("Log Data") created by your use of the Services. Log Data may include information such as your IP address, browser type, operating system, the referring web page, pages visited, location, your mobile carrier, device, search terms, and cookie information. We receive Log Data when you interact with our Services, for example, when you visit our websites, sign into our Services, or interact with our email notifications. We use Log Data to provide our Services and to measure, customize, and improve them, as well as collect and use them in the aggregate. This Policy does not apply to, nor do we take any responsibility for any information that is collected either by any third party using the Website or through any links on the Website or through any of the advertisements.
+          Cookies are small data files that we transfer to your computer. We use "session" cookies to keep you logged in while you use our Services, to better understand how you interact with our Services, and to monitor aggregate usage and web traffic information on our Services. We use "persistent" cookies to recognize you each time you return to our Services. For example, we create a persistent cookie that includes some basic information about you, like your most recent search and whether an agent responded to a query you sent using Sigma Tenant's Services. We use this persistent cookie to remember your preferences and, if you create an account, to make your user experience consistent after you register.
+          Most internet browsers automatically accept cookies, but you can change your settings or use third-party tools to refuse cookies or prompt you before accepting cookies from the websites you visit. You can also use your browser settings or other tools to delete cookies you already have. Please be aware that some parts of our Services may not work for you if you disable cookies.
+          Information We Collect from REPs: If you create a REP account with us, whether as a landlord, agent, developer or subscriber we will ask you to provide certain information in order to set up your account. We ask all REPs to provide a name, a company (if available), a phone number, and an email address. If you create a paid REP account and become a subscriber, we require you to submit all of the information required from landlords and agents as well as a description of your campaign. We will also collect financial data like credit card information and billing addresses from subscribers, so that we can handle billing. If you are a subscriber, a Sigma Tenant representative may call you to verify your information.
+          Additional Information About You that We Obtain from Third Parties: We may sometimes obtain information about you from third parties and use it to help agents serve you better. For example, our partners may help us provide predictions of your income range and how likely you may be to purchase a home. We will share this augmented data only with selected REPs (subscribers) who you choose to share information with by contacting them through the Sigma Tenant Services.
+          Location Data: If you provide location information (like a ZIP code, street address, or even just a city) during the registration process or at any other time via your account settings, we will store that information and associate it with your account.
+          We may use and store information about your location to provide localized features, to analyze our Services with the help of third-party analytics services, to provide you with relevant advertising and marketing materials, and otherwise to improve and customize our Services.
+          
+          <Text style = { styles.innercontent }>
+            {'\n'}{'\n'}How We Share Your Information?{'\n'}{'\n'}
+          </Text>
+          
+          As you use, access and navigate our Website, we collect various information, which may either be personally identifiable information or information that is not personally identifiable. This information is classified as:
+          “Personal Information” which means personally identifiable information like your name, your address, your age, your email address and your phone number.
+          “Sensitive Personal Information” which means your financial information, your passwords, any biometric, medical or health related information.
+          “Other Information” which means all other information other than “Personal Information” or “Sensitive Personal Information.” For example, the nature of device information, your usage patterns, your usage preferences, logs which are created when you access and use the Website, local caches on your browser, your location, ip address and the like. These are just examples and the list is by no means exhaustive.
+          Any personal information will be used to allow you to login to your account or to resolve specific service issues, inform you of our new services or features and to communicate with you in relation to your use of the Website. Any other information we collect will be used for -
+          Our business purposes and this may include the viewing or advertising of residential or commercial real estate, rental properties or to use associated services;
+          Analytical purposes, data usage;
+          Improving the Website, or your user experience; and
+          Providing targeted advertisements to you.
+          We will share all information we collect from you, including the personal information with any third party if we in good faith believe it is required for the purposes of our compliance with any applicable law, regulation, legal process or enforceable governmental request/action or any manner required for the purposes of protecting our rights or interests.
+          From time to time, we make the e-mail addresses of those who access our Website available to other reputable organizations whose products or services we think you might find interesting. If you do not want us to share your e-mail address with other companies or organizations, please let us know by emailing us at info@SigmaTenant.
+          Persons who supply us with their telephone numbers on-line may receive telephone or text message contact from us with information regarding new products and services or upcoming events. If you do not wish to receive such telephone calls or text messages, please let us know by e-mailing us at info@Sigma Tenant
+          With respect to Ad Servers: To try and bring you offers that are of interest to you, we have relationships with other companies that we allow to place ads on our Web pages. As a result of your visit to our Website, ad server companies may collect information such as your domain type, your IP address and clickstream information.
+          Non-Personally Identifiable Information (Non-PII): is information that cannot by itself be used to identify an individual person. Non-PII includes information received from the web browser, operating system, or device, including such things as information about the browser and browser settings, information about the operating system or device, the unique identifier assigned to a device, the web address of a web page visited, the name or type of mobile app, the time and date a web page or app was visited, the address of the referring web page, and other information about a web page or app. It could also include the number of times a particular browser or device has been shown a particular ad, or it may include generic categories about users and their interests (for example, “male flat buyer”). Non-PII also includes information about the precise geographic location of the device, which may be collected when the user of the device permits such collection.
+          Use of Non-PII: In order to provide and improve our Services and to share relevant ads with you, provide interest-based advertising (to opt out of interest-based advertising on the Platform, go to the Opt Out section below), show ads in a particular sequence, customize ads to a particular geographic region, determine when users respond to ads and reporting aggregated statistics regarding, for example, the effectiveness of online advertising campaigns we use and disclose to our advertising and analytics partners Non-PII that we collect, including cookie data, some log data, and mobile data. We also share anonymous profiles of users who view a property with REPs.
+          The user must send a mail to opt out of tracking Non PII on all our online properties stating your property and the user mail sent via your host login.
+          Opting out of tracking on our services through this link inserts a cookie on the user’s browser. We rely on this cookie to identify an opted-out user. We will not be able to identify an opted out user, if the user
+          Clears the cookie from his/her browser,
+          Uses a different browser, Or
+          Uses a different computer
+          We will discontinue tracking activities for an opted out user. This means that we will not track an opted out user’s behaviour or display customised ads to the user. However, an opted out user may be able to view suggestions by us pertaining to marketing, branding or promotional campaigns.
+          We currently allow you to request the changes made to your information only on a special request via the contact mail. The original details will still be with us in our database for analysis and use.
+          User-Generated Content: If you use our user-generated content services, like posting a question, you should be aware that any personally identifiable information you submit there can be read, collected, or used by other users and could be used to send you unsolicited messages. We are not responsible for the personally identifiable information you choose to submit in these forums.
+          Protecting Ourselves and Our Users: We may release personal information when we believe that doing so is appropriate to comply with a legitimate law, regulation, or legal request; to enforce or apply our policies; to initiate, render, bill, and collect for amounts owed to us; to protect our rights or property; to protect the safety of our users; to address fraud, security, or technical issues; or if we reasonably believe that an emergency involving immediate danger of death or serious physical injury to any person requires disclosure of communications or justifies disclosure of records without delay.
+          
+          <Text style = { styles.innercontent }>
+          {'\n'}{'\n'}General Matters{'\n'}{'\n'}
+          </Text>
+
+          The Accuracy and Confidentiality of Your Account Information Is Your Responsibility: You are responsible for maintaining the secrecy and accuracy of your password, email address, and other account information at all times. We recommend a strong password that you do not use with other services. We are not responsible for personal data transmitted to a third party as a result of an incorrect email address.
+          Third-Party Websites: Our Website may contain links to other websites. When you click on one of these links, you are navigating to another website. We do not accept liability for misuse of any information by any website controller to which we may link. We also do not have any responsibility of liability for the content on websites that use the Sigma Tenant search functionality. We encourage you to read the privacy statements of these websites, which may differ from ours.
+          Sale/Merger: Information about our Users and our Website is a business asset of Sigma Tenant. Consequently, information about our Users, including personal information, will be disclosed as part of any merger or acquisition, creation of a separate business to provide the Website, our Services or fulfill products, sale or pledge of company assets as well as in the event of an insolvency, bankruptcy or receivership in which personal information would be transferred as one of the business assets of the company.
+          Security: We use commercially reasonable security measures to protect the loss, misuse, and alteration of the information under our control. However, we cannot absolutely guarantee the protection of any information shared with us. When you enter sensitive information (such as a credit card number) on our order forms, we encrypt the transmission of that information using secure socket layer technology (SSL).
+          Policy Changes: We may update this Privacy Policy with np prior intimation and also to reflect changes to our information practices. You can check the changed policies on the policy page in our website.
+          
+          <Text style = { styles.innercontent }>
+          {'\n'}{'\n'}Thank You for Your Time!
+          </Text>
+        </Text>
+      </View>
+    </ScrollView>
+
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    margin: 10,
+    height: '100%',
+  },
+
+  heading1: {
+    fontSize: 25,
+    textAlign: 'center',
+    paddingTop: 20,
+    fontWeight: 'bold'
+  },
+
+  heading2: {
+    fontSize: 20,
+    width: '70%',
+    paddingTop: 20,
+    fontWeight: 'bold'
+  },
+
+  maincontent: {
+    lineHeight: 23,
+    fontSize: 17
+  },
+
+  innercontent: {
+    fontSize: 19,
+    fontWeight: 'bold'
+  }
+});
